d6c0f18b6d9019b54ae693ec43bcfe65c1d4d57a
ChangChang <bbmini96@hanmail.net>
2024-05-31 10:13:23 +0900

    출석부 만들기 ing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
+import Student from './components/Student';
+
+
+const initialAttendanceInfo = {
+  count: 1,
+  students: [
+    {
+      id: 1,
+      name: 'IT',
+      isAbsent: false
+    }
+  ]
+}
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [attendanceInfo, setAttendanceInfo] = useState(initialAttendanceInfo);
+
+  const studentTotal = useRef(1);
+  const currentStudentId = useRef(1);
+
+
+
+  const onAdd = () => {
+
+    const presentStudent = {
+      id: currentStudentId.current += 1,
+      name: name,
+      isAbsent: false
+    }
+
+    setAttendanceInfo({
+      count: ++studentTotal.current,
+      students: [
+        ...attendanceInfo.students,presentStudent
+      ]
+    })
+    
+  }
+
+
+  const onDelete = (id) => {
+        setAttendanceInfo({
+          count: studentTotal.current -1,
+          students: attendanceInfo.students.filter(
+            (student) => student.id !== id
+          )
+        })
+  }
+
+  const onUpdate = (id) => {
+    setAttendanceInfo({
+      count:studentTotal.current,
+      students: attendanceInfo.students.map(
+        (student) =>  student.id ===id?{...student,isAbsent:!student.isAbsent}:student
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Attendance</h1>
+      <p>Attendance List Total : {attendanceInfo.count}</p>
+
+
+      <div>
+        <input
+          type='text'
+          placeholder='Input the student name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+       
+        <button onClick={() => onAdd()}>Add</button>
+
+      </div>
+      <div>
+        {attendanceInfo.students.map((student) => {
+          return <Student 
+                  key={student.id} 
+                  name={student.name} 
+                  id ={student.id} 
+                  isAbsent={student.isAbsent}
+                  onDelete={onDelete} 
+                  onUpdate={onUpdate}                  
+          />;
+        })}
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Student.js
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+
+
+
+
+export default function Student({ id, name, onDelete, onUpdate, isAbsent }) {
+    return(
+        <div>
+            <span onClick={() => onUpdate(id)} style={{
+                textDecoration: isAbsent? 'line-through':'none',
+                color: isAbsent? 'red':'black'
+            }}>{name}</span> &nbsp;&nbsp;
+            <span><button onClick={ () => onDelete(id)}> X </button>
+            </span>
+        </div>
+    )
+}
