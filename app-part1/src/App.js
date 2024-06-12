01f3e9cb2da207ef22b2637adefcd8a3020f82e6
Changchang <bbmini96@hanmail.net>
2024-06-12 16:35:40 +0900

    app.js update

--- app-part1/src/App.js
+++ app-part1/src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+// import './App.css';
 import {useEffect, useState} from 'react';
 // import { fetchNotice } from './api/fetchData';
 
@@ -7,14 +7,22 @@ function App() {
   const [notice, setNotice] = useState([]);
 
   useEffect(() =>{
-    fetch('http://localhost:8080/api/notice')
+    let page = 5;
+    fetch(`http://localhost:8080/api/notice?page=${page}`)
     .then((response) => response.json())
-    .then((data)=>console.log(data))
+    .then((data)=>setNotice(data.content))
   },[])
 
 
   return (
     <div className="App">
+      <ul>
+        {notice && notice.map((notice) => (
+          <li key={notice.no}>
+            {notice.no}. {notice.title} - {notice.content}
+          </li>
+        ))}
+      </ul>
 
     </div>
   );
