81573923897a36d938cbd7c735b04ca3019c2be9
Changchang <bbmini96@hanmail.net>
2024-06-12 14:35:02 +0900

    app-part1 생성

--- my-dog/src/App.js
+++ my-dog/src/App.js
@@ -1,28 +1,37 @@
-import React, { useNavigate } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
-// import styled from 'styled-components'
-import { Printing} from "./components/Printing";
+import { Printing } from "./components/Printing";
 import { About } from "./components/About";
-// import { Link } from "react-router-dom";
-// import { Route, Router, Routes, json } from 'react-router-dom';
+import styled from "styled-components";
 
 
 
 function App() {
-
+  const navigate = useNavigate();
  
   return (
     <div className="App">
         <h1 className='title'>Welcome, Dog World</h1>
-
-        <About />
         <div className='btn-div'>
-          <button className='btn-About' onClick={() => <About />}>About</button>
-          <button className='btn-Print' onClick={() => <Printing />}>Printing</button>
+          <button className='btn-About' onClick={() => navigate('/about')}>About</button>
+          <button className='btn-Print' onClick={() => navigate('/printing')}>Printing</button>
         </div>
     </div>
 
   );
 }
 
-export default App;
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/printing" element={<Printing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
