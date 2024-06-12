bda346169e20e3f3ba58cbfaf809688457173048
Changchang <bbmini96@hanmail.net>
2024-06-12 14:35:34 +0900

    app-part1 업데이트

--- app-part1/src/App.js
+++ app-part1/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react';
+// import { fetchNotice } from './api/fetchData';
 
 function App() {
+  const [notice, setNotice] = useState([]);
+
+  useEffect(() =>{
+    fetch('http://localhost:8080/api/notice')
+    .then((response) => response.json())
+    .then((data)=>console.log(data))
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ app-part1/src/api/fetchData.js
@@ -0,0 +1,8 @@
+
+// export function fetchNotice(){
+//     const result = fetchNotice('http://localhost:8080/api/notice')
+//                     .then((response) => response.json());
+
+//     return result;
+
+// }
